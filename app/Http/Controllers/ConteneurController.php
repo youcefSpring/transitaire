b27a429a17a6d3610809ef6eba0cd181023f778f
youcefSpring <youcef12091997@gmail.com>
2026-09-01 19:17:44 +0100

    work in fix mss erros

--- app/Http/Controllers/ConteneurController.php
+++ app/Http/Controllers/ConteneurController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ConteneurStoreRequest;
+use App\Http\Requests\ConteneurUpdateRequest;
+use App\Models\Client;
 use App\Models\Conteneur;
+use App\Models\Dossier;
 use App\Services\AuditService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
